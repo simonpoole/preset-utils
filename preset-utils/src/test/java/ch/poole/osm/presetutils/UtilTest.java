a71ec2350e898dde31fac33ef58396e879d94081
simonpoole <simon@poole.ch>
2017-11-18 15:07:34 +0100

    First separate commit

---
+++ preset-utils/src/test/java/ch/poole/osm/presetutils/UtilTest.java
@@ -0,0 +1,11 @@
+package ch.poole.osm.presetutils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UtilTest {
+    @Test public void testSomeLibraryMethod() {
+//        ch.poole.osm.presetutils.Preset2Pot.main(args);
+//        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
