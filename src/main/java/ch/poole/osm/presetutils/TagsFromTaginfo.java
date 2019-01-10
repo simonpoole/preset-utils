f1bc4ccdffe3da472100fec47e737b6f68cd526b
simonpoole <simon@poole.ch>
2019-01-10 17:52:36 +0100

    Add ford as top level tag

--- src/main/java/ch/poole/osm/presetutils/TagsFromTaginfo.java
+++ src/main/java/ch/poole/osm/presetutils/TagsFromTaginfo.java
@@ -45,7 +45,7 @@ public class TagsFromTaginfo {
      * of such.
      */
     public static final Set<String>         OBJECT_KEYS       = Collections.unmodifiableSet(
-            new HashSet<>(Arrays.asList("aerialway", "aeroway", "amenity", "barrier", "boundary", "building", "craft", "emergency", "geological", "highway",
+            new HashSet<>(Arrays.asList("aerialway", "aeroway", "amenity", "barrier", "boundary", "building", "craft", "emergency", "ford", "geological", "highway",
                     "historic", "landuse", "leisure", "man_made", "military", "natural", "office", "place", "power", "public_transport", "railway", "shop",
                     "tourism", "waterway", "type", "entrance", "pipeline", "healthcare", "playground", "attraction", "traffic_sign", "traffic_sign:forward",
                     "traffic_sign:backward", "golf", "indoor", "cemetry", "building:part", "landcover", "advertising")));
