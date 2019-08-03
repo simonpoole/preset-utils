ea2ea9fd1f5fbb4684ef64edd123ed826accaa54
simonpoole <simon@poole.ch>
2019-08-03 15:23:20 +0200

    Handle addTags properly

--- src/main/java/ch/poole/osm/presetutils/ID2JOSM.java
+++ src/main/java/ch/poole/osm/presetutils/ID2JOSM.java
@@ -262,6 +262,37 @@ public class ID2JOSM {
     static class Tag {
         String key;
         String value;
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((key == null) ? 0 : key.hashCode());
+            result = prime * result + ((value == null) ? 0 : value.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Tag other = (Tag) obj;
+            if (key == null) {
+                if (other.key != null)
+                    return false;
+            } else if (!key.equals(other.key))
+                return false;
+            if (value == null) {
+                if (other.value != null)
+                    return false;
+            } else if (!value.equals(other.value))
+                return false;
+            return true;
+        }
     }
 
     static class Item {
@@ -313,19 +344,33 @@ public class ID2JOSM {
                 writer.print("\" ");
             }
             writer.println((!searchable ? "deprecated=\"true\" " : "") + "preset_name_label=\"true\">");
-            if (tags != null) {
-                for (Tag tag : tags) {
-                    if (tag.key != null && !"".equals(tag.key) && !tag.key.contains("*")) {
-                        indent(writer, 2);
-                        if (tag.value != null && !"".equals(tag.value) && !tag.value.contains("*")) {
-                            writer.println("<key key=\"" + StringEscapeUtils.escapeXml11(tag.key) + "\" value=\"" + StringEscapeUtils.escapeXml11(tag.value)
-                                    + "\" />");
-                        } else { // generate a text field
-                            writer.println("<text key=\"" + StringEscapeUtils.escapeXml11(tag.key) + "\" />");
+            List<Tag> tempTags = (tags == null ? new ArrayList<Tag>() : new ArrayList<>(tags));
+
+            // try to keep top level tags on top
+            if (addTags != null) {
+                for (Tag tag : addTags) {
+                    if (!(tags.contains(tag))) {
+                        if (Tags.OBJECT_KEYS.contains(tag.key)) {
+                            tempTags.add(0, tag);
+                        } else {
+                            tempTags.add(tag);
                         }
                     }
                 }
             }
+
+            for (Tag tag : tempTags) {
+                if (tag.key != null && !"".equals(tag.key) && !tag.key.contains("*")) {
+                    indent(writer, 2);
+                    if (tag.value != null && !"".equals(tag.value) && !tag.value.contains("*")) {
+                        writer.println(
+                                "<key key=\"" + StringEscapeUtils.escapeXml11(tag.key) + "\" value=\"" + StringEscapeUtils.escapeXml11(tag.value) + "\" />");
+                    } else { // generate a text field
+                        writer.println("<text key=\"" + StringEscapeUtils.escapeXml11(tag.key) + "\" />");
+                    }
+                }
+            }
+
             if (fields != null) {
                 for (Field field : fields) {
                     if (chunkMode) {
